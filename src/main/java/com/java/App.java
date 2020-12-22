4f6efcc04a64f42add32818d43684e5d5845aed9
tw19961113 <cosin.867196621@qq.com>
2020-12-22 18:05:45 +0800

    Initial commit

---
+++ src/main/java/com/java/App.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * by TanWei 2020/12/22
+ **/
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
---
+++ src/main/java/com/java/controller/Hello.java
@@ -0,0 +1,7 @@
+package com.java.controller;
+
+/**
+ * by TanWei 2020/12/22
+ **/
+public class Hello {
+}
